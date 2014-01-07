c26de6fd636396e946fc633770c42f6c8e9fb902
Chip Senkbeil <chip.senkbeil@gmail.com>
2014-01-07 01:35:40 -0500

    Moved code to other functions

--- main.c
+++ main.c
@@ -6,43 +6,75 @@
 #include <string.h>
 #include <stdio.h>
 
+#define MAX_DEVICES 32 /* Maximum number of devices marked as event# */
+#define SET_DEVICE_PATH(INDEX,STR) sprintf(STR, "/dev/input/event%d", INDEX)
+
 static const char *const evval[3] = {
     "RELEASED",
     "PRESSED ",
     "REPEATED"
 };
 
-int main(void)
+/*
+ * Opens a device.
+ *
+ * index: The index of the device to open
+ *
+ * Return: The file descriptor or -1 if failed
+ */
+int open_device(int index)
 {
-    const char *dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-    struct input_event ev;
-    ssize_t n;
-    int fd;
+    int fd; {
+        char path[256];
+        SET_DEVICE_PATH(index, path);
 
-    fd = open(dev, O_RDONLY | O_NONBLOCK);
-    if (fd == -1) {
-        fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
-        return EXIT_FAILURE;
+        if ((fd = open(path, O_RDONLY | O_NONBLOCK)) < 0)
+            fprintf(stderr, "Cannot open %s: %s.\n", 
+                path, strerror(errno));
     }
 
-    while (1) {
-        n = read(fd, &ev, sizeof ev);
+    return fd;
+}
 
-        if (n == (ssize_t)-1) {
-            if (errno == EINTR)
-                continue;
-            else if (errno == EAGAIN)
-                continue;
-            else
-                break;
-        } else
-        if (n != sizeof ev) {
-            errno = EIO;
-            break;
-        }
+/*
+ *
+ */
+int event_occurred(int f, struct input_event* event)
+{
+    ssize_t n;
+    n = read(f, event, sizeof *event);
 
-       if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2)
+    // Ignore potential errors and pass false as return
+    if (n == (ssize_t) -1) return 0;
+    if (n != sizeof *event) { return 0; }
+
+    return 1;
+}
+
+/*
+ *
+ */
+int event_is_keyboard(struct input_event* event)
+{
+    return (event->type == EV_KEY && event->value >= 0 && event->value <= 2);
+}
+
+// ============================================================================
+// = MAIN PROGRAM
+// ============================================================================
+
+int main(int argc, char** argv)
+{
+    struct input_event ev;
+    int fd;
+
+    if ((fd = open_device(2)) < 0)
+        return EXIT_FAILURE;
+
+    for(;;) {
+        if (event_occurred(fd, &ev) && event_is_keyboard(&ev)) {
             printf("%s 0x%04x (%d)\n", evval[ev.value], (int)ev.code, (int)ev.code);
+        }
     }
 
     fflush(stdout);
